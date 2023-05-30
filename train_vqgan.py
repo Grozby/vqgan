dc1ae032ec71fa3e3f6a949a66dce81525a3f4cf
Grozby <gabriele.aldeghi.27@gmail.com>
2023-05-30 22:48:46 +0100

    fix(TrainVQGAN): general fixes, separate tapes for vqvae and discriminator

--- train_vqgan.py
+++ train_vqgan.py
@@ -14,12 +14,7 @@ class TrainVQGAN(tf.keras.Model):
         *args,
         vqgan_kwargs: Dict,
         discriminator_kwargs: Dict,
-        epochs: int,
-        batch_size: int,
-        learning_rate_vqgan: float,
-        learning_rate_discriminator: float,
-        warmup_steps_percentage: float,
-        total_number_images: int,
+        lpips_weights_path: str,
         discriminator_step_start: int,
         perceptual_loss_factor: float = 1.,
         reconstruction_loss_factor: float = 1.,
@@ -29,12 +24,6 @@ class TrainVQGAN(tf.keras.Model):
         super().__init__(*args, **kwargs)
         self.vqgan_kwargs = vqgan_kwargs
         self.discriminator_kwargs = discriminator_kwargs
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.learning_rate_vqgan = learning_rate_vqgan
-        self.learning_rate_discriminator = learning_rate_discriminator
-        self.warmup_steps_percentage = warmup_steps_percentage
-        self.total_number_images = total_number_images
         self.discriminator_step_start = discriminator_step_start
         self.perceptual_loss_factor = perceptual_loss_factor
         self.reconstruction_loss_factor = reconstruction_loss_factor
@@ -42,51 +31,33 @@ class TrainVQGAN(tf.keras.Model):
 
         self.vqgan = VQGAN(**vqgan_kwargs)
         self.discriminator = Discriminator(**discriminator_kwargs)
-        self.discriminator.apply(weights_init)
-        self.perceptual_loss = LPIPS("vqgan/losses/lpips_weights.h5")
+        self.perceptual_loss = LPIPS(lpips_weights_path)
 
         self.loss_vqgan_tracker, self.loss_discriminator_tracker = (
             tf.keras.metrics.Mean(name="loss_vqgan"),
-            tf.keras.metrics.Mean(name="loss_vqgan"),
+            tf.keras.metrics.Mean(name="loss_discriminator"),
         )
 
         self._step = tf.Variable(0, dtype=tf.int64)
 
         self.optimizer_vqgan = None
-        self.opt_discriminator = None
-
-        # total_steps = (total_number_images // batch_size) * epochs
-        # warmup_steps = int(warmup_steps_percentage * total_steps)
-
-        # self.optimizer_vqgan = tf.keras.optimizers.Adam(
-        #     learning_rate=WarmUpCosineDecay(
-        #         learning_rate=learning_rate_vqgan,
-        #         warmup_steps=warmup_steps,
-        #         total_steps=total_steps,
-        #         hold_steps=0,
-        #     ),
-        #     epsilon=1e-08,
-        # )
-        # self.opt_discriminator = tf.keras.optimizers.Adam(
-        #     learning_rate=learning_rate_discriminator,
-        #     epsilon=1e-08,
-        # )
+        self.optimizer_discriminator = None
 
     def compile(
         self,
         *args,
         optimizer_vqgan,
-        opt_discriminator,
+        optimizer_discriminator,
         **kwargs,
     ):
         super().compile()
         self.optimizer_vqgan = optimizer_vqgan
-        self.opt_discriminator = opt_discriminator
+        self.optimizer_discriminator = optimizer_discriminator
 
     @property
     def discriminator_loss_factor(self) -> float:
-        return (self._discriminator_loss_factor
-                if self._step.value() > self.discriminator_step_start else 0.)
+        return tf.where(self._step.value() > self.discriminator_step_start,
+                        self._discriminator_loss_factor, 0.)
 
     def _calculate_lambda(self, perceptual_loss, gan_loss):
         last_layer_weights = self.vqgan.decoder.output_convolution.weights
@@ -99,8 +70,8 @@ class TrainVQGAN(tf.keras.Model):
             last_layer_weights,
         )[0]
 
-        lambda_value = tf.norm(perceptual_loss_grads, ord="fro")
-        lambda_value /= (tf.norm(gan_loss_grads, ord="fro") + 1e-4)
+        lambda_value = tf.norm(perceptual_loss_grads)
+        lambda_value /= (tf.norm(gan_loss_grads) + 1e-4)
         lambda_value = tf.clip_by_value(
             lambda_value,
             clip_value_min=0,
@@ -115,14 +86,13 @@ class TrainVQGAN(tf.keras.Model):
             decoded_images, _, q_loss = self.vqgan(images)
 
             # Run Discriminator
-            discriminator_real = self.discriminator(images)
             discriminator_fake = self.discriminator(decoded_images)
 
             # Run LPIPS
-            perceptual_loss = self.perceptual_loss(
+            perceptual_loss = self.perceptual_loss([
                 images,
                 decoded_images,
-            )
+            ])
             reconstruction_loss = tf.abs(images - decoded_images)
 
             pr_loss = tf.reduce_mean(
@@ -132,12 +102,7 @@ class TrainVQGAN(tf.keras.Model):
 
             lambda_value = self._calculate_lambda(pr_loss, g_loss)
             vq_loss = (pr_loss + q_loss +
-                       self.discriminator_factor * lambda_value * g_loss)
-
-            d_loss_real = tf.reduce_mean(tf.nn.relu(1. - discriminator_real))
-            d_loss_fake = tf.reduce_mean(tf.nn.relu(1. + discriminator_fake))
-            discriminator_loss = (self.discriminator_loss_factor * 0.5 *
-                                  (d_loss_real + d_loss_fake))
+                       self.discriminator_loss_factor * lambda_value * g_loss)
 
         vqgan_gradients = tape.gradient(
             vq_loss,
@@ -148,6 +113,16 @@ class TrainVQGAN(tf.keras.Model):
                 vqgan_gradients,
                 self.vqgan.trainable_variables,
             ))
+
+        with tf.GradientTape() as tape:
+            # Run Discriminator
+            discriminator_real = self.discriminator(images)
+            discriminator_fake = self.discriminator(decoded_images)
+
+            d_loss_real = tf.reduce_mean(tf.nn.relu(1. - discriminator_real))
+            d_loss_fake = tf.reduce_mean(tf.nn.relu(1. + discriminator_fake))
+            discriminator_loss = (self.discriminator_loss_factor * 0.5 *
+                                  (d_loss_real + d_loss_fake))
 
         discriminator_gradients = tape.gradient(
             discriminator_loss,
